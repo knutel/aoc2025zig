e84fbec06802ec6ebc8b61e99c3ff4ca0fc10fd2
Knut Eldhuset <knut.eldhuset@gmail.com>
2025-12-01 18:18:13 +0100

    Day 1 - one ugly mess

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,107 @@
 const std = @import("std");
-const aoc2025zig = @import("aoc2025zig");
+const aoc2025zig = @import("root.zig");
+
+pub fn newPosition(current: i32, code: aoc2025zig.Code) struct { i32, u8 } {
+    var pos = current;
+    var wraps: u8 = 0;
+    if (code.dir == .left) {
+        var steps = code.steps;
+        while (pos - steps < 0) {
+            steps -= 100;
+            wraps += 1;
+        }
+        pos -= steps;
+        if (current == 0) {
+            wraps -= 1;
+        }
+    } else {
+        var steps = code.steps;
+        while (pos + steps > 99) {
+            steps -= 100;
+            wraps += 1;
+        }
+        pos += steps;
+        if (pos == 0) {
+            wraps -= 1;
+        }
+    }
+
+    return .{ pos, @as(u8, wraps) };
+}
+
+test "newPosition basic tests" {
+    const pos1 = newPosition(50, aoc2025zig.Code{ .dir = .left, .steps = 68 });
+    try std.testing.expect(pos1[0] == 82);
+    try std.testing.expect(pos1[1] == 1);
+
+    const pos2 = newPosition(82, aoc2025zig.Code{ .dir = .left, .steps = 30 });
+    try std.testing.expect(pos2[0] == 52);
+    try std.testing.expect(pos2[1] == 0);
+
+    const pos3 = newPosition(52, aoc2025zig.Code{ .dir = .right, .steps = 48 });
+    try std.testing.expect(pos3[0] == 0);
+    try std.testing.expect(pos3[1] == 0);
+
+    const pos4 = newPosition(0, aoc2025zig.Code{ .dir = .left, .steps = 5 });
+    try std.testing.expect(pos4[0] == 95);
+    try std.testing.expect(pos4[1] == 0);
+
+    const pos5 = newPosition(95, aoc2025zig.Code{ .dir = .right, .steps = 60 });
+    try std.testing.expect(pos5[0] == 55);
+    try std.testing.expect(pos5[1] == 1);
+
+    const pos6 = newPosition(55, aoc2025zig.Code{ .dir = .left, .steps = 55 });
+    try std.testing.expect(pos6[0] == 0);
+    try std.testing.expect(pos6[1] == 0);
+
+    const pos7 = newPosition(0, aoc2025zig.Code{ .dir = .left, .steps = 1 });
+    try std.testing.expect(pos7[0] == 99);
+    try std.testing.expect(pos7[1] == 0);
+
+    const pos8 = newPosition(99, aoc2025zig.Code{ .dir = .left, .steps = 99 });
+    try std.testing.expect(pos8[0] == 0);
+    try std.testing.expect(pos8[1] == 0);
+
+    const pos9 = newPosition(0, aoc2025zig.Code{ .dir = .right, .steps = 14 });
+    try std.testing.expect(pos9[0] == 14);
+    try std.testing.expect(pos9[1] == 0);
+
+    const pos10 = newPosition(14, aoc2025zig.Code{ .dir = .left, .steps = 82 });
+    try std.testing.expect(pos10[0] == 32);
+    try std.testing.expect(pos10[1] == 1);
+}
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try aoc2025zig.bufferedPrint();
-}
+    // const lines = try aoc2025zig.readFileLines(std.heap.page_allocator, "input_test_01_01.txt");
+    // const lines = try aoc2025zig.readFileLines(std.heap.page_allocator, "input_test_01_02.txt");
+    const lines = try aoc2025zig.readFileLines(std.heap.page_allocator, "input_01.txt");
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    const codes = try aoc2025zig.parseDay1(std.heap.page_allocator, lines);
+    var position: i32 = 50;
+    var zeroes: i32 = 0;
+    var part2: i32 = 0;
+    std.debug.print("Current Position: {d}\n", .{position});
+    std.debug.print("Hit zero position! Total zeroes: {d}\n", .{zeroes});
+    std.debug.print("Part 2 total increments: {d}\n", .{part2});
+    for (codes.items) |code| {
+        std.debug.print("Direction: {s}, Steps: {d}\n", .{
+            switch (code.dir) {
+                .left => "Left",
+                .right => "Right",
+            },
+            code.steps,
+        });
+        const new_pos, const new_wraps = newPosition(position, code);
+        position = new_pos;
+        part2 += @as(i32, new_wraps);
+        if (position == 0) {
+            zeroes += 1;
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+        std.debug.print("Current Position: {d}\n", .{position});
+        std.debug.print("Hit zero position! Total zeroes: {d}\n", .{zeroes});
+        std.debug.print("Part 2 total increments: {d}\n", .{part2});
+    }
+    std.debug.print("Hit zero position! Total zeroes: {d}\n", .{zeroes});
+    std.debug.print("Part 2 total increments: {d}\n", .{part2});
+    std.debug.print("Final Position: {d}\n", .{part2 + zeroes});
 }
--- src/root.zig
+++ src/root.zig
@@ -21,3 +21,72 @@ pub fn add(a: i32, b: i32) i32 {
 test "basic add functionality" {
     try std.testing.expect(add(3, 7) == 10);
 }
+
+pub const Direction = enum {
+    left,
+    right,
+};
+
+pub const Code = struct {
+    dir: Direction,
+    steps: i32,
+};
+
+pub fn parseDay1(gpa: std.mem.Allocator, lines: std.ArrayList([]const u8)) !std.ArrayList(Code) {
+    var result: std.ArrayList(Code) = .empty;
+    // for (lines.items) |line| {
+    //     std.debug.print("ParseLine: {s}\n", .{line});
+    // }
+    for (lines.items) |line| {
+        var dir: Direction = undefined;
+        // std.debug.print("Parsing line: {s}\n", .{line});
+        if (line[0] == 'L') {
+            dir = .left;
+        } else if (line[0] == 'R') {
+            dir = .right;
+        } else {
+            return error.InvalidDirection;
+        }
+        // std.debug.print("Parsing line: {s}\n", .{line[1..]});
+        const steps = try std.fmt.parseInt(i32, line[1..], 10);
+        try result.append(gpa, Code{ .dir = dir, .steps = steps });
+    }
+
+    return result;
+}
+
+pub fn readFileLines(gpa: std.mem.Allocator, path: []const u8) !std.ArrayList([]const u8) {
+    var file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+
+    var file_buffer: [4096]u8 = undefined;
+    var reader = file.reader(&file_buffer);
+    var line_no: usize = 0;
+    var lines: std.ArrayList([]const u8) = .empty;
+    while (try reader.interface.takeDelimiter('\n')) |line| {
+        line_no += 1;
+        // std.debug.print("{d}--{s}\n", .{ line_no, line });
+        if (line.len == 0) continue; // Skip empty lines
+        const l = try gpa.alloc(u8, line.len);
+        std.mem.copyForwards(u8, l, line);
+        try lines.append(gpa, l);
+    }
+    return lines;
+}
+
+test "parseDay1 works" {
+    //    const allocator = std.testing.allocator;
+    const allocator = std.heap.page_allocator;
+    var input: std.ArrayList([]const u8) = .empty;
+    try input.append(std.heap.page_allocator, "L2");
+    try input.append(std.heap.page_allocator, "R3");
+    try input.append(std.heap.page_allocator, "L10");
+    const codes = try parseDay1(allocator, input);
+    try std.testing.expect(codes.items.len == 3);
+    try std.testing.expect(codes.items[0].dir == .left);
+    try std.testing.expect(codes.items[0].steps == 2);
+    try std.testing.expect(codes.items[1].dir == .right);
+    try std.testing.expect(codes.items[1].steps == 3);
+    try std.testing.expect(codes.items[2].dir == .left);
+    try std.testing.expect(codes.items[2].steps == 10);
+}
