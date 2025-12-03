1b2ac4a5d3618ae12c836a9afbe7bed24cc51325
Knut Eldhuset <knut.eldhuset@gmail.com>
2025-12-03 07:07:08 +0100

    Day 3, part 1

---
+++ src/day03.zig
@@ -0,0 +1,83 @@
+const std = @import("std");
+const aoc2025zig = @import("root.zig");
+
+pub fn largestDigit(startPos: usize, string: []const u8) struct { u8, usize } {
+    var largest: u8 = 0;
+    var pos: usize = startPos;
+    for (startPos..string.len) |i| {
+        const c = string[i] - '0';
+        if (c > largest) {
+            largest = c;
+            pos = i;
+        }
+    }
+    return .{ largest, pos };
+}
+
+test "largestDigit tests" {
+    var result = largestDigit(0, "123456789");
+    try std.testing.expect(result[0] == 9);
+    try std.testing.expect(result[1] == 8);
+    result = largestDigit(3, "123456789");
+    try std.testing.expect(result[0] == 9);
+    try std.testing.expect(result[1] == 8);
+    result = largestDigit(5, "987654321");
+    try std.testing.expect(result[0] == 4);
+    try std.testing.expect(result[1] == 5);
+    result = largestDigit(8, "000000009");
+    try std.testing.expect(result[0] == 9);
+    try std.testing.expect(result[1] == 8);
+}
+
+pub fn findLargestJoltage(line: []const u8) u64 {
+    var largest = largestDigit(0, line);
+    var first: u8 = 0;
+    var second: u8 = 0;
+
+    if (largest[1] == line.len - 1) {
+        second = largest[0];
+        largest = largestDigit(0, line[0..largest[1]]);
+        first = largest[0];
+    } else {
+        first = largest[0];
+        largest = largestDigit(largest[1] + 1, line);
+        second = largest[0];
+    }
+    std.debug.print("Line: {s}, first: {d}, second: {d}\n", .{ line, first, second });
+    const joltage = first * 10 + second;
+    return joltage;
+}
+
+pub fn solve() !void {
+    // const lines = try aoc2025zig.readFileLines(std.heap.page_allocator, "input_test_03_01.txt");
+    // const lines = try aoc2025zig.readFileLines(std.heap.page_allocator, "input_test_03_02.txt");
+    try solveWithFile(std.heap.page_allocator, "input_03.txt");
+}
+
+pub fn solveWithFile(allocator: std.mem.Allocator, path: []const u8) !void {
+    var lines = try aoc2025zig.readFileLines(allocator, path);
+    defer {
+        for (lines.items) |line| {
+            allocator.free(line);
+        }
+        lines.deinit(allocator);
+    }
+
+    var sum: u64 = 0;
+    var sum2: u64 = 0;
+
+    sum2 += 1;
+
+    for (lines.items) |line| {
+        const joltage = findLargestJoltage(line);
+        std.debug.print("Joltage: {d}\n", .{joltage});
+        sum += joltage;
+    }
+
+    std.debug.print("Day 3, Part 1: {d}\n", .{sum});
+    std.debug.print("Day 3, Part 2: {d}\n", .{sum2});
+}
+
+test "day3 solve test" {
+    try solveWithFile(std.testing.allocator, "input_test_03_01.txt");
+}
